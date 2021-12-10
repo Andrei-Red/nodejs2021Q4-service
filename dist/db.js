bbca1ef1719a61b87ee0ca79bc69895ecdf2b6b1
Andrei_Dzenicenko <andrei_dzenicenko@epam.com>
2021-12-10 20:57:54 +0300

    del: deleted dist

--- dist/db.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.db = void 0;
-exports.db = {
-    users: [],
-    boards: [],
-    tacks: [],
-    columns: [],
-};
