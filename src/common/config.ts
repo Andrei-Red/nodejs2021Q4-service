5e1cd69f097e45d07da4155d2ee0fc139c3a5365
Andrei_Dzenicenko <andrei_dzenicenko@epam.com>
2021-12-11 12:08:29 +0300

    fix: ts lint

--- src/common/config.ts
+++ src/common/config.ts
@@ -13,13 +13,19 @@ type TConfig = {
     AUTH_MODE: boolean | string;
 }
 
-export const config: TConfig = {} as TConfig
+export const config: TConfig = {
+    PORT: process.env.PORT as string,
+    NODE_ENV: process.env.PORT as string,
+    MONGO_CONNECTION_STRING: process.env.PORT as string,
+    JWT_SECRET_KEY: process.env.PORT as string,
+    AUTH_MODE: process.env.PORT as string,
+} as TConfig
 
-config['PORT'] = process.env.PORT as string;
-config['NODE_ENV'] = process.env.PORT as string;
-config['MONGO_CONNECTION_STRING'] = process.env.PORT as string;
-config['JWT_SECRET_KEY'] = process.env.PORT as string;
-config['AUTH_MODE'] = process.env.PORT as string;
+// config['PORT'] = process.env.PORT as string;
+// config['NODE_ENV'] = process.env.PORT as string;
+// config['MONGO_CONNECTION_STRING'] = process.env.PORT as string;
+// config['JWT_SECRET_KEY'] = process.env.PORT as string;
+// config['AUTH_MODE'] = process.env.PORT as string;
 
 
 // module.exports = {
