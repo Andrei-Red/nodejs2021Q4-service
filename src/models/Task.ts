fa11dcfd7cb64ba6412585c203f4e539d12bb27c
Andrei_Dzenicenko <andrei_dzenicenko@epam.com>
2022-01-19 12:54:28 +0300

    feat: connect to postgres

---
+++ src/models/Task.ts
@@ -0,0 +1,39 @@
+const { v4 } = require('uuid');
+
+export interface ITask {
+  id: string;
+  title: string;
+  order: number;
+  description: string;
+  userId: string | null;
+  boardId: string | null;
+  columnId: string | null;
+}
+
+class Task implements ITask {
+  id: string;
+
+  title: string;
+
+  order: number;
+
+  description: string;
+
+  userId: string | null;
+
+  boardId: string | null;
+
+  columnId: string | null;
+
+  constructor(task: ITask = {} as ITask) {
+    this.id = v4();
+    this.title = task.title;
+    this.order = task.order;
+    this.description = task.description;
+    this.userId = task.userId;
+    this.boardId = task.boardId;
+    this.columnId = task.columnId;
+  }
+}
+
+module.exports = Task;
---
+++ src/server.ts
@@ -0,0 +1,57 @@
+import "reflect-metadata";
+import { config } from './common/config';
+// import  ormconfig   from './common/ormconfig';
+import { createConnection } from 'typeorm';
+
+const Koa = require('koa');
+const koaBody = require('koa-body');
+const routerUser = require('./router/userRouter');
+const routerBoard = require('./router/boardRouret');
+const routerTask = require('./router/taskRouter');
+
+const {
+  handlerError,
+  handlerErrorAfterRouters,
+  handlerCodeError,
+} = require('./loggerManager/otherError');
+
+try {
+  process.on('unhandledRejection', (reason) => {
+    handlerCodeError('Error in inside code', { reason });
+  });
+
+  process.on('uncaughtException', (reason) => {
+    handlerCodeError('Error in inside code', { reason });
+  });
+
+  const server = new Koa();
+
+  server.use(handlerError);
+  server.use(koaBody());
+
+  server.use(routerUser.routes());
+  server.use(routerUser.allowedMethods());
+  server.use(routerBoard.routes());
+  server.use(routerBoard.allowedMethods());
+  server.use(routerTask.routes());
+  server.use(routerTask.allowedMethods());
+
+  server.use(handlerErrorAfterRouters);
+
+
+  createConnection()
+    .then(() => {
+      console.log('createConnection SUCCESS');
+      server.listen(config.PORT, () =>
+        console.log(`App is running on http://localhost:${config.PORT}`)
+      );
+    })
+    .catch((error: Error) => {
+      process.stderr.write(`${error.name}`);
+      process.exit(1);
+    });
+
+
+} catch (e) {
+  handlerCodeError('Error in inside code', { e });
+}
