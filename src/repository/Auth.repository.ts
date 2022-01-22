afba509ea67a0ec2d8b81a45abe698f53f50ef13
Andrei_Dzenicenko <andrei_dzenicenko@epam.com>
2022-01-22 21:02:35 +0300

    feat: add auth repo

---
+++ src/repository/Auth.repository.ts
@@ -0,0 +1,13 @@
+import { getRepository } from 'typeorm';
+import { User } from '../entities/User';
+
+
+export const get = async (login: string): Promise<User | boolean>  => {
+  const user =  await getRepository(User).findOne({login})
+
+  if(!user) {
+    return false
+  }
+
+  return user
+}
